c403c5339e6b593674c4165aa92d6858f672a55b
Ubuntu <George@funcVM.20au2ui4qakenlkbdo5ntj5uha.zx.internal.cloudapp.net>
2020-12-10 15:20:06 +0000

    finished

--- service1/__init__.py
+++ service1/__init__.py
@@ -1,30 +1,33 @@
 import logging
 import requests
 import azure.functions as func
-from azure.cosmos import CosmosClient
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
+
+
+endpoint = "https://george-rhodes.documents.azure.com:443/"
+key = "kILpCMZAwpelpDZMAxawxHM7pgETsCi4kiWY1qB9XZMP3CaHfCw74LUEZeWMwsLtEEmd6Vfmi6iMcJuvEyghIg=="
+client = CosmosClient(endpoint, key)
+
+database_name = "user"
+database=client.create_database_if_not_exists(id=database_name)
+
+container_name = "User"
+container = database.create_container_if_not_exists(id=container_name, partition_key=PartitionKey(path="/username"), offer_throughput=400)
+
+
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     letterstring = requests.get('https://randostring.azurewebsites.net/api/service2?code=0PIZjZ5krmmlkIeoW1vwD40ezvlnUUWgIQYe3kLneP/BAoAkTa0TDA==').text
-    nostring = requests.get('https://randostring.azurewebsites.net/api/service2?code=0PIZjZ5krmmlkIeoW1vwD40ezvlnUUWgIQYe3kLneP/BAoAkTa0TDA==').text
+    numstring = requests.get('https://randostring.azurewebsites.net/api/service3?code=Dpsg85A2oaWewTtawMoAS9q6vObDsbF/1WvHBaasrr7Si9QJEFBPGQ==').text
     
     username = ""
     for i in range(5):
         username += letterstring[i]
-        username += nostring[i]
+        username += numstring[i]
     
-    endpoint = "https://george-rhodes.documents.azure.com:443/"
-    key = "kILpCMZAwpelpDZMAxawxHM7pgETsCi4kiWY1qB9XZMP3CaHfCw74LUEZeWMwsLtEEmd6Vfmi6iMcJuvEyghIg=="
-    client = CosmosClient(endpoint, key)
-
-    database_name = "name"
-    client.create_database_if_not_exists(id=database_name)
-
-    container_name = "UserContainer"
-    container = database.create_container_if_not_exists(
-        id=container_name,
-        partition_key=PartitionKey(path="/username")
-    )
+   
     username_to_add = {
         "id": username
     }
